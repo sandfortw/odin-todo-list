f11f6d73fa90555c43139bf0172ba51abf58da5c
Weston Sandfort <sandfortw@gmail.com>
2024-03-30 16:26:19 -0600

    Congigure webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'To-do-list Inbox',
+      inject:'head',
+      scriptLoading: 'defer'
+    })
+  ]
+};
