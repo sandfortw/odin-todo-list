56f93215f7277782747e28acb582791d922a2141
Weston Sandfort <sandfortw@gmail.com>
2024-04-01 20:29:18 -0600

    Create add-project button in nav

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './style.css';
-import addTaskButton from './createAddProjectButton.js'
 
 
 let addTaskContainer = document.querySelector('#add-task-button')
-addTaskContainer.appendChild(addTaskButton)
 
 const dialogBox = document.querySelector('#add-task-dialog-box')
 const dialogCloseButton = document.querySelector('#dialog-close')
