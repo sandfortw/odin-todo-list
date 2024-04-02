e780111aeaaefb948f15fa0a88ae271ac2d1e667
Weston Sandfort <sandfortw@gmail.com>
2024-04-01 20:36:24 -0600

    Create add-project-button which opens a dialog box and closes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import './style.css';
+const taskDialogBox = document.querySelector('#add-task-dialog-box')
+const taskDialogCloseButton = document.querySelector('#task-dialog-close')
+const addTaskContainer = document.querySelector('#add-task-button')
+const addProjectContainer = document.querySelector('#add-project-container')
+const projectDialogBox = document.querySelector('#add-project-dialog-box')
 
+const projectDialogCloseButton = document.querySelector('#project-dialog-close')
 
-let addTaskContainer = document.querySelector('#add-task-button')
-
-const dialogBox = document.querySelector('#add-task-dialog-box')
-const dialogCloseButton = document.querySelector('#dialog-close')
 
 addTaskContainer.addEventListener('click', ()=>{
-  dialogBox.showModal()
+  taskDialogBox.showModal()
 })
 
-dialogCloseButton.addEventListener('click', ()=>{
-  dialogBox.close()
+taskDialogCloseButton.addEventListener('click', ()=>{
+  taskDialogBox.close()
 })
+
+addProjectContainer.addEventListener('click', ()=>{
+  projectDialogBox.showModal()
+})
+
+projectDialogCloseButton.addEventListener('click', ()=>{
+  projectDialogBox.close()
+})
+
+
+
+
+
+
+
