bed113024305e8e12027fd7e261ea0a3698da072
Weston Sandfort <sandfortw@gmail.com>
2024-04-01 20:20:55 -0600

    Add task button opens a dialog box

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import addTaskButton from './createAddProjectButton.js'
 
 let addTaskContainer = document.querySelector('#add-task-button')
 addTaskContainer.appendChild(addTaskButton)
+
+const dialogBox = document.querySelector('#add-task-dialog-box')
+const dialogCloseButton = document.querySelector('#dialog-close')
+
+addTaskContainer.addEventListener('click', ()=>{
+  dialogBox.showModal()
+})
+
+dialogCloseButton.addEventListener('click', ()=>{
+  dialogBox.close()
+})
